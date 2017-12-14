9b28833ffd1c4ebf512de85153c2be251f9e5f10
Laurence <liuxy6@gmail.com>
2017-12-14 15:33:52 -0500

    BitsReader

--- base_chinese.go
+++ base_chinese.go
@@ -28,74 +28,12 @@ func main() {
 		}
 	}
 
-	encode(table, bufio.NewReader(os.Stdin), os.Stdout)
+	Encode(table, bufio.NewReader(os.Stdin), os.Stdout)
 }
 
-type BitReader struct {
-	in     io.ByteReader
-	n_bits uint // <= 64
-
-	buf    uint16
-	offset uint
-	eof    bool
-}
-
-func (self *BitReader) fillBuffer() {
-	for !self.eof && self.offset <= 8 {
-		b, err := self.in.ReadByte()
-		if err != nil {
-			if err == io.EOF {
-				self.eof = true
-			} else {
-				log.Fatalln(err)
-			}
-		}
-
-		self.offset += 8
-		self.buf &= 0xff << (16 - self.offset)
-		self.buf += uint16(b) << (8 - self.offset)
-	}
-}
-
-// n <= self.offset
-func (self *BitReader) popNBits(n uint) uint64 {
-	ret := uint64(self.buf) >> (16 - 1 - n)
-	self.offset -= n
-	return ret
-}
-
-func (self *BitReader) Read() (uint64, bool) {
-	var ret uint64 = 0
-	n_bits := self.n_bits
-
-	for n_bits > 0 {
-		n := min(n_bits, self.offset)
-		ret <<= n
-		ret += self.popNBits(n)
-		n_bits -= n
-
-		if !self.eof {
-			self.fillBuffer()
-		} else {
-			break
-		}
-	}
-
-	if n_bits > 0 && (self.offset > 0 || !self.eof) {
-		log.Fatalln("Error!")
-	}
-
-	if n_bits > 0 {
-		ret <<= n_bits
-		return ret, true
-	} else {
-		return ret, false
-	}
-}
-
-func encode(table []rune, in io.ByteReader, out io.Writer) {
+func Encode(table []rune, in io.ByteReader, out io.Writer) {
 	n_bits := max_bits_represented(len(table))
-	reader := BitReader{in: in, n_bits: n_bits}
+	reader := NewBitsReader(in, n_bits)
 
 	for {
 		index, eof := reader.Read()
